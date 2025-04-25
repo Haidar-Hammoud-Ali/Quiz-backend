5d25c026bc832d9f17e1ed14731dfc8ad16efb2f
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-25 21:43:38 +0300

    Saving the variables for registration table and hashing the password

--- register.php
+++ register.php
@@ -1,3 +1,7 @@
 require_once 'config/database.php';
 
 $data = json_decode(file_get_contents("php://input"));
+
+$name=$data-> name;
+$email=$data-> email;
+password=$data -> password_hash($data->password, PASSWORD_DEFAULT);
