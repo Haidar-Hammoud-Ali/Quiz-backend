c9012c176d45b6f81e975114668111c0bdeb8ebf
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-25 21:59:01 +0300

    Preparing and excuting into sql in this case table users

--- register.php
+++ register.php
@@ -5,3 +5,9 @@ $data = json_decode(file_get_contents("php://input"));
 $name=$data-> name;
 $email=$data-> email;
 password=$data -> password_hash($data->password, PASSWORD_DEFAULT);
+
+try {
+    $sql= "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    $stmt= $pdo->prepare ($sql);
+    $stmt->execute ([$name, $email, $password]);
+}
