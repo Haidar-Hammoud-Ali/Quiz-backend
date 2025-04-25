c68558aa7b93e96ee906295e818dd1f8c1b3baed
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-25 12:19:12 +0300

    Connecting php to mysql using php data object

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$database = 'quiz_app';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;db=$database", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
