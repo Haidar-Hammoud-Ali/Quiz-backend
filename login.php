c46d24444336800e1b18df8052ff835d1525d22b
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-25 23:16:09 +0300

    Searching for the input by email

--- login.php
+++ login.php
@@ -4,4 +4,14 @@ require_once 'config/database.php';
 $data = json_decode (file_get_contents("php://input"));
 $email = $data ->email;
 $password = $data ->password;
+
+try{
+    $sql= "SELECT * FROM users WHERE email =?";
+    $stmt= $pdo ->prepare ($sql);
+    $stmt -> execute ([$email]);
+    $user= $stmt ->fetch (PDO::FETCH_ASSOC);
+
+
+
+}
 ?>
