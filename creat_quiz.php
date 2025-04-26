7c81898cde596c0e1dd88011ec7c996bffc5c10d
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-26 22:56:15 +0300

    Inserting quize title and user , reply success message

--- creat_quiz.php
+++ creat_quiz.php
@@ -4,4 +4,12 @@ require_once 'config/database.php';
 $data = json_decode(file_get_contents("php://input"));
 $title = $data->title;
 $user_id = $data->user_id;
+
+try {
+    $sql = "INSERT INTO quizzes (title, user_id) VALUES (?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$title, $user_id]);
+
+    echo json_encode(["message" => "Quiz created"]);
+} 
 ?>
