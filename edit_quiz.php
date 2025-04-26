14903f32249a6ede06f0a6a81dfdee55b56f00e1
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-26 23:52:09 +0300

    Ctaching error for quize edit

--- edit_quiz.php
+++ edit_quiz.php
@@ -4,4 +4,12 @@ $data = json_decode(file_get_contents("php://input"));
 
 $quiz_id = $data->quiz_id;
 $new_title = $data->new_title;
+
+try {
+    $sql = "UPDATE quizzes SET title = ? WHERE id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$new_title, $quiz_id]);
+}catch(PDOException $e) {
+    echo json_encode(["error" => $e->getMessage()]);
+}
 ?>
