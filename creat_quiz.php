888f31bc1a67b7d889f9f1fa632feec06844c7e3
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-27 12:51:54 +0300

    Creating standard creat quiz code format

--- creat_quiz.php
+++ creat_quiz.php
@@ -5,13 +5,9 @@ $data = json_decode(file_get_contents("php://input"));
 $title = $data->title;
 $user_id = $data->user_id;
 
-try {
-    $sql = "INSERT INTO quizzes (title, user_id) VALUES (?, ?)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$title, $user_id]);
+$sql = "INSERT INTO quizzes (title, user_id) VALUES (?, ?)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$title, $user_id]);
 
-    echo json_encode(["message" => "Quiz created"]);
-} catch (PDOException $e) {
-    echo json_encode(["error" => $e->getMessage()]);
-}
+echo json_encode(["message" => "Quiz created"]);
 ?>
