586dd85ce64f80abf7c326289773bb8e55f06846
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-04-27 14:18:23 +0300

    Deleting question by id

--- delete_question.php
+++ delete_question.php
@@ -4,5 +4,8 @@ require_once 'config/database.php';
 $userdata= file_get_contents("php://input");
 $userdata = json_decode($userdata);
 
+$questionid=$userdata->questionid;
 
+$deletquestion= "DELETE FROM questions where id = ?";
+$
 ?>
